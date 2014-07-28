5874476a2166c0d45f1411bd93a0442af26f859d
Imanol <eltigrequehayenti@gmail.com>
2014-07-28 23:11:57 +0200

    Refactor from while to for

--- src/com/blokura/primefactors/PrimeFactors.java
+++ src/com/blokura/primefactors/PrimeFactors.java
@@ -12,15 +12,13 @@ public class PrimeFactors {
         List<Integer> primes = new ArrayList<Integer>();
         int candidate = 2;
         while (n > 1) {
-            while (n % candidate == 0) {
+            for (; n % candidate == 0; n /= candidate) {
                 primes.add(candidate);
-                n /= candidate;
             }
+            
             candidate++;
         }
-        if (n > 1) {
-            primes.add(n);
-        }
+
         return primes;
     }
 
