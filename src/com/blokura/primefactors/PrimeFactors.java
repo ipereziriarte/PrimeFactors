257309765dc61476ab5bf01236d4e7174597504b
Imanol <eltigrequehayenti@gmail.com>
2014-07-28 23:08:07 +0200

    Passing test 9

--- src/com/blokura/primefactors/PrimeFactors.java
+++ src/com/blokura/primefactors/PrimeFactors.java
@@ -11,11 +11,12 @@ public class PrimeFactors {
     public static List<Integer> generate(int n) {
         List<Integer> primes = new ArrayList<Integer>();
         int candidate = 2;
-        if (n > 1) {
+        while (n > 1) {
             while (n % candidate == 0) {
                 primes.add(candidate);
                 n /= candidate;
             }
+            candidate++;
         }
         if (n > 1) {
             primes.add(n);
